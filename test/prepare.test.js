2383c77961e5e3d0c9c5d197653ab1632d7ef141
Tania Mathern <mathern@adobe.com>
2019-11-05 09:25:15 -0800

    Added first failing tests

---
+++ test/prepare.test.js
@@ -0,0 +1,33 @@
+/**
+ *  ADOBE CONFIDENTIAL
+ *  __________________
+ *
+ *  Copyright 2018 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of Adobe Systems Incorporated and its suppliers,
+ *  if any.  The intellectual and technical concepts contained
+ *  herein are proprietary to Adobe Systems Incorporated and its
+ *  suppliers and are protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Adobe Systems Incorporated.
+ */
+
+/* eslint-env mocha */
+/* eslint mocha/no-mocha-arrows: "off" */
+
+'use strict';
+
+//const mockFs = require('mock-fs');
+//const fs = require('fs-extra');
+//const nock = require('nock');
+const assert = require('assert');
+
+
+describe('prepare tests', () => {
+    it('just fails', () => {
+        assert.fail();
+    });
+});
