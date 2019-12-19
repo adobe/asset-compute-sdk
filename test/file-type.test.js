6edee6b257c31ba237bb6c2d7e44077e0e5c0349
Tania Mathern <mathern@adobe.com>
2019-12-19 12:25:52 -0800

    Initial failing test

---
+++ test/file-type.test.js
@@ -0,0 +1,30 @@
+/**
+ *  ADOBE CONFIDENTIAL
+ *  __________________
+ *
+ *  Copyright 2019 Adobe Systems Incorporated
+ *  All Rights Reserved.
+ *
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of Adobe Systems Incorporated and its suppliers,
+ *  if any.  The intellectual and technical concepts contained
+ *  herein are proprietary to Adobe Systems Incorporated and its
+ *  suppliers and are protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Adobe Systems Incorporated.
+ */
+
+/* eslint-env mocha */
+/* eslint mocha/no-mocha-arrows: "off" */
+
+'use strict';
+
+const assert = require('assert');
+const FileTypeChecker = require('../lib/utils/file-type.js');
+
+describe("file-type.js", function (){
+    it("just fails", function(){
+
+    });
+});
