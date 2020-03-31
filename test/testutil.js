7cc6f8a7e91c64b38fe12c126ea0343384487492
Alexander Klimetschek <aklimets@adobe.com>
2020-03-30 21:02:42 -0700

    disable all http instrumentation to fix hanging tests
    The issue was that the new setTimeout() handler added in the latest
    @nui/openwhisk-newrelic 5.0.0 when a response is not read is very often
    triggered by the tests here (api.js, http multipart, shell script).
    It seems they don't seem to always read the response properly, and
    use of nock might also be a factor.
    
    As a simple fix, I added env vars to completely disable the
    instrumentation. It isn't really needed at all here.

--- test/testutil.js
+++ test/testutil.js
@@ -48,6 +48,10 @@ function beforeEach() {
 
     process.env.__OW_ACTION_NAME = "/namespace/package/test_action";
     process.env.NUI_DISABLE_RETRIES = "disable";
+
+    // http instrumentation has timeouts that can get in the way and we don't need it for any of the tests
+    process.env.OPENWHISK_NEWRELIC_DISABLE_ALL_INSTRUMENTATION = true;
+
     mockFs();
 
     MetricsTestHelper.beforeEachTest();
