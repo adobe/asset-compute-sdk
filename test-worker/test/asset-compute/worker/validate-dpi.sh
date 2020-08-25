c16d90449c28d63d08d787836d8fbc48dd1183dc
Jamie Delbick <jamie.delbick@gmail.com>
2020-08-25 11:45:03 -0700

    move post-processing code back to sdk (#45)
    * initial move image processing over
    
    * fix mocks
    
    * fix mock stop, rename post-processing, fix eslint
    
    * fix copyright
    
    * disable codecov commit status
    
    * export image post process as a function
    
    Co-authored-by: Alexander Klimetschek <aklimets@adobe.com>

--- test-worker/test/asset-compute/worker/validate-dpi.sh
+++ test-worker/test/asset-compute/worker/validate-dpi.sh
@@ -1,44 +1,27 @@
+#!/bin/bash
+
 # Copyright 2020 Adobe. All rights reserved.
 # This file is licensed to you under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License. You may obtain a copy
 # of the License at http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software distributed under
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 # OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-language: node_js
+# echo all commands for verbose logs
+set -x
 
-node_js:
-  - "10"
-  - "10.15"
-  - "10.19"
-  - "12"
-  - "14"
+# exit when any command fails
+set -e
 
-install:
-  - npm version
-  - npm install
-  - npm install -g @adobe/aio-cli
+$(dirname $0)/../validate-image.sh $1 $2
 
-jobs:
-  include:
-    - stage: Test
-      script: npm test
-    - stage: Release
-      if: branch = master
-      node_js: "12"
-      script: skip
-      before_deploy:
-        - "echo 'semantic release...'"
-      deploy:
-        on:
-          branch: master
-          condition:  $TRAVIS_OS_NAME == linux
-        provider: script
-        skip_cleanup: true
-        script: npm run semantic-release
-
-after_success:
-  - npm run report-coverage
+# check for equal dpi
+imgDpiExpected=$(identify -format "%x x %y" $1)
+imgDpiActual=$(identify -format "%x x %y" $2)
+if [[ "$imgDpiActual" != "$imgDpiExpected" ]]; then
+    echo "dpi not equal: $imgDpiActual instead of expected $imgDpiExpected"
+    exit 4
+fi
