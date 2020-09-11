833037323e2c136833381a425c61f478131af880
Alexander Klimetschek <aklimets@adobe.com>
2020-09-11 10:28:27 -0700

    FEATURE-RELEASE: NUI-665 Watermarking feature
    * add watermark worker test
    
    * improve bounding box handling
    
    * use getAsset to download watermark
    
    * fix failing source tests
    
    * add error conditions and review comment
    
    * add watermark test with orientation
    
    * add test for corrupt watermark
    
    * add test coverage to storage.js
    
    * add unit tests for math.js
    
    * fix comments
    
    * use typeof to determine if dimensions are a number
    
    * fix indentation
    
    * fix quotes
    
    * remove old watermark api
    
    Co-authored-by: Krishnadas Menon <krmenon@adobe.com>
    Co-authored-by: delbick <delbick@adobe.com>

--- test/source.test.js
+++ test/source.test.js
@@ -19,75 +19,63 @@ const assert = require('assert');
 const Source = require('../lib/source');
 
 describe("source.js", () => {
-    it('verifies name and path with source as string and with no directory', function() {
+    it('verifies path with no name and no directory', function() {
+        const source = new Source({});
+        assert.strictEqual(source.name, "");
+        assert.strictEqual(source.path, ".");
+    });
+
+    it('verifies path with no name and a directory', function() {
+        const source = new Source({}, "/tmp");
+        assert.strictEqual(source.name, "");
+        assert.strictEqual(source.path, "/tmp");
+    });
+    it('verifies path with name and no directory and empty source object', function() {
         const sourceName = 'test-source.png';
-        const source = new Source(sourceName);
+        const source = new Source({}, undefined, sourceName);
         assert.strictEqual(source.name, 'test-source.png');
         assert.strictEqual(source.path, 'test-source.png');
+        assert.strictEqual(source.url, undefined);
+        assert.strictEqual(source.type, undefined);
     });
 
-    it('verifies name and path with source as string and with a directory with trailing slash', function () {
+    it('verifies path with name and source object but no directory', function() {
         const sourceName = 'test-source.png';
-        const source = new Source(sourceName, "/");
+        const sourceObj = {
+            url: 'https://example.com',
+            type: 'type'
+        };
+        const source = new Source(sourceObj, undefined, sourceName);
+        assert.strictEqual(source.name, 'test-source.png');
+        assert.strictEqual(source.path, 'test-source.png');
+        assert.strictEqual(source.url, 'https://example.com');
+        assert.strictEqual(source.type, 'type');
+    });
+
+
+    it('verifies path with name and with a directory with trailing slash', function() {
+        const sourceName = 'test-source.png';
+        const sourceObj = {
+            url: 'https://example.com',
+            type: 'type'
+        };
+        const source = new Source(sourceObj, '/', sourceName);
         assert.strictEqual(source.name, 'test-source.png');
         assert.strictEqual(source.path, '/test-source.png');
+        assert.strictEqual(source.url, 'https://example.com');
+        assert.strictEqual(source.type, 'type');
     });
 
-    it('verifies name and path with source as string and with a directory with no trailing slash', function () {
+    it('verifies path with name and with a directory with no trailing slash', function() {
         const sourceName = 'test-source.png';
-        const source = new Source(sourceName, "/tmp");
+        const sourceObj = {
+            url: 'https://example.com',
+            type: 'type'
+        };
+        const source = new Source(sourceObj, '/tmp', sourceName);
         assert.strictEqual(source.name, 'test-source.png');
         assert.strictEqual(source.path, '/tmp/test-source.png');
-    });
-
-    it('verifies name and path with source as empty string', function () {
-        const sourceName = '';
-        const source = new Source(sourceName);
-        assert.strictEqual(source.name, '');
-    });
-    it('verifies name with source as an object', function() {
-        const source = {};
-        assert.strictEqual(new Source(source).name, 'source');
-        source.name = 'abcdz-AZ1234567890.jpg';
-        assert.strictEqual(new Source(source).name, `source.jpg`);
-        source.name =  `  %789.PSD`;
-        assert.strictEqual(new Source(source).name, `source.PSD`);
-        source.name =  `!@#$%^&*().png`;
-        assert.strictEqual(new Source(source).name, `source.png`);
-        source.name = '';
-        assert.strictEqual(new Source(source).name, 'source');
-    });
-    it('verifies name using mimeType', function() {
-        const source = { };
-        source.name = 'abcdz-AZ1234567890';
-        source.mimeType = 'image/jpeg';
-        assert.strictEqual(new Source(source).name, `source.jpeg`);
-        source.name =  '';
-        source.mimeType = 'unknown mimeType';
-        assert.strictEqual(new Source(source).name, 'source');
-        source.name = 'foo.png';
-        source.mimeType = 'image/jpeg';
-        assert.strictEqual(new Source(source).name, `source.png`);
-    });
-    it('verifies name with source a url', function() {
-        const source = { url: ''};
-        source.url = 'https://server.name/file.jpg?queryPortion';
-        assert.strictEqual(new Source(source).name, `source.jpg`);
-        source.url = 'http://server.name/directory/file%20.png?query';
-        assert.strictEqual(new Source(source).name, `source.png`);
-        source.url = 'http://server.name/directory/file%20.png?';
-        assert.strictEqual(new Source(source).name, `source.png`);
-        source.url = 'xxx://server.name/directory/file.png?query';
-        assert.strictEqual(new Source(source).name, `source.png`);
-        source.url = 'NotAUrl';
-        assert.strictEqual(new Source(source).name, 'source');
-        source.url='';
-        assert.strictEqual(new Source(source).name, 'source');
-        source.mimeType ='image/png';
-        assert.strictEqual(new Source(source).name, `source.png`);
-    });
-    it('verifies name with empty source object', function() {
-        const source = { };
-        assert.strictEqual(new Source(source).name, 'source');
+        assert.strictEqual(source.url, 'https://example.com');
+        assert.strictEqual(source.type, 'type');
     });
 });
