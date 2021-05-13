29d146d837f2e445255191893de183eddc79d63e
delbick <delbick@adobe.com>
2021-05-12 17:23:33 -0700

    mob next [ci-skip]

--- test-worker/worker.js
+++ test-worker/worker.js
@@ -26,7 +26,7 @@ process.env.SDK_POST_PROCESSING_TEST_MODE = true;
 process.env.DEBUG = "*";
 
 exports.main = worker(async (source, rendition) => {
-    console.log("In test-worker rendition callback")
+    console.log("In test-worker rendition callback", rendition)
 
     const instructions = rendition.instructions;
 
